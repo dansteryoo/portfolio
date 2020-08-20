55317186c8c89bb469cb03816132c7f3ec250ab1
Danny Yoo <dansteryoo@gmail.com>
2020-08-20 03:17:14 -0400

    update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21b5e0c7b15d0bdfc4910b2130c9b755.js"
+  "/precache-manifest.a4f240501b8f9bafa74db0e345c04e93.js"
 );
 
 self.addEventListener('message', (event) => {
