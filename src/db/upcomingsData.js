a4c2e3a8ea2d03afa334b9d76baca1e9a2455ad9
Danny Yoo <dansteryoo@gmail.com>
2021-12-26 17:29:27 -0500

    add off the pulpit

--- src/db/upcomingsData.js
+++ src/db/upcomingsData.js
@@ -4,6 +4,14 @@ export const upcomingsData = [
 		description: 'The website for New Church of Atlanta English Ministry',
 		githubUrl: '#',
 		techItems: ['React', 'Material UI', 'Firebase', 'React Hook Form', 'Yup'],
-		siteUrl: 'https://newchurchatlem.netlify.app/',
+		siteUrl: 'https://newchurchatlem.netlify.app',
+	},
+	{
+		name: 'Off the Pulpit Podcast',
+		description:
+			'Three pastors & friends conversing about church, culture, and life off the pulpit.',
+		githubUrl: '#',
+		techItems: ['React', 'Nextjs', 'Vercel', 'Firebase'],
+		siteUrl: 'https://offthepulpitpod.vercel.app',
 	},
 ];
